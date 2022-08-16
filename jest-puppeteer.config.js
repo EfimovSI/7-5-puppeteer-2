e3f8cf6c40fa219293b90454fff05f67430a5e8e
Sergey Efimov <yefimovsergey00@gmail.com>
2022-08-16 22:34:18 +0300

    First task of puppeteer2 completed

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   launch: {
     // здесь можем указывать все глобальные параметры запуска браузера для функции launch()
-    slowMo: 20,
-    headless: false,
+    // slowMo: 50,
+    // headless: false,
     defaultViewport: null,
     args: ['--start-maximized'] // используем максимальный размер окна браузера
   }
